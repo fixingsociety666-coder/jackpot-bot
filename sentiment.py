ca7a634d02bd1dcb229c33888abcafeb854e502e
fixingsociety666-coder <fixingsociety666@gmail.com>
2025-08-31 19:28:09 -0400

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,5 +1,10 @@
 from textblob import TextBlob
 
 def analyze_sentiment(text):
+    """
+    Returns sentiment polarity:
+    >0 positive, <0 negative, 0 neutral
+    """
     blob = TextBlob(text)
-    return round(blob.sentiment.polarity, 2)
+    polarity = blob.sentiment.polarity
+    return polarity
