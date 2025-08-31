1aac084c7cf136ce070cafbe34c67fb2e19c1830
fixingsociety666-coder <fixingsociety666@gmail.com>
2025-08-31 18:13:55 -0400

    Delete polygon_data.py

--- polygon_data.py
+++
@@ -1,9 +0,0 @@
-import requests
-from config import POLYGON_API_KEY
-
-def get_current_price(symbol):
-    url = f"https://api.polygon.io/v2/aggs/ticker/{symbol}/prev?apiKey={POLYGON_API_KEY}"
-    r = requests.get(url).json()
-    if "results" in r:
-        return r["results"][0]["c"]
-    return None
